12be9b313b2d50630598fbd4aea0ab52c0f9c185
unknown <pjuned.it@gmail.com>
2025-05-04 00:12:06 +0530

    OK

--- redis.sh
+++ redis.sh
@@ -33,34 +33,58 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-#dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $LOGFILE
+# #dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $LOGFILE
 
-dnf install remi-release-8.rpm --skip-broken -y
-
-
-#validate $? "installing redis repo package"
-
-dnf module enable redis:remi-6.2 -y 
+# dnf install remi-release-8.rpm --skip-broken -y
 
 
-validate $? "enabling redis 6.2" 
+# #validate $? "installing redis repo package"
 
-dnf install redis -y 
+# dnf module enable redis:remi-6.2 -y 
 
-validate $? "installing redis"
+
+# validate $? "enabling redis 6.2" 
+
+# dnf install redis -y 
+
+# validate $? "installing redis"
+
+# sec -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
+
+
+# validate $? "allowing remote connections"
+
+# systemctl enable redis 
+
+# validate $? "enabling resis"
+
+# systemctl start redis 
+
+# validate $? "starting redis"
+
+
+#!/bin/bash
+
+# Enable EPEL and Remi repos
+dnf install -y epel-release
+dnf install -y https://rpms.remirepo.net/enterprise/remi-release-8.rpm
+
+# Enable Redis module from Remi repo
+dnf module enable -y redis:remi-6.2
+
+# Install Redis
+dnf install -y redis
 
 sec -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
 
 
 validate $? "allowing remote connections"
 
-systemctl enable redis 
 
-validate $? "enabling resis"
+# Start and enable Redis
+systemctl enable redis
+systemctl start redis
 
-systemctl start redis 
-
-validate $? "starting redis"
-
-
+# Check Redis status
+systemctl status redis
 
