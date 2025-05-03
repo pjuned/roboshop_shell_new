a255f6dd1c25d6fe1a18d22dc30d0704c972eda6
unknown <pjuned.it@gmail.com>
2025-05-03 23:57:34 +0530

    OK

--- redis.sh
+++ redis.sh
@@ -33,7 +33,10 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-dnf install -y https://rpms.remirepo.net/enterprise/remi-release-8.rpm &>> $LOGFILE
+#dnf install -y https://rpms.remirepo.net/enterprise/remi-release-8.rpm &>> $LOGFILE
+
+curl -O https://rpms.remirepo.net/enterprise/remi-release-8.rpm
+
 
 validate $? "installing redis repo package"
 
